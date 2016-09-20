b1465acbd6cf0a81eafa19f1162b1030707048b7
arotena <albertosss27@gmail.com>
2016-09-20 20:12:50 +0200

    ejemplo

---
+++ prueba.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+    
+    def test_factorial(self):
+        self.assertEqual(1, factorial(1))
+
+def factorial(n):
+    return 1 if n < 1 else n * factorial(n-1)
+
+if __name__ == '__main__':
+    unittest.main()
