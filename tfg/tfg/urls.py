2f456865de2de344a219264cae11ecbedd6c3b14
arotena <albertosss27@gmail.com>
2017-05-10 20:21:03 +0200

    modificado

--- tfg/tfg/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url, patterns, include
-from django.contrib import admin
-
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$' , 'inforepo.views.introurl'),
-    url(r'.*' , 'inforepo.views.notFound')
-]
