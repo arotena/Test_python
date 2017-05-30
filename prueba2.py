8bfbbfb2a3857c80bb17daeeed9749b022044e6d
arotena <albertosss27@gmail.com>
2017-05-30 20:03:43 +0200

    modificado

--- prueba2.py
+++ prueba2.py
@@ -1,14 +1,15 @@
 import hashlib
 import unittest 
 class Foo(object):
- def gravatar_url(self, email):  hash = hashlib.md5(email.lower()).hexdigest()  return "https://www.gravatar.com/avatar/" + hash
+	def gravatar_url(self, email):  
+		hash = hashlib.md5(email.lower()).hexdigest()  
+		return "https://www.gravatar.com/avatar/" + hash
 class FooTestCase(unittest.TestCase):
- def setUp(self):
-  self.foo = Foo()
- 
- def test_get_gravatar_url(self):
-  result = self.foo.gravatar_url("example1@gmail.com")
-  self.assertEquals(result, "https://www.gravatar.com/avatar/f3e820cc128ffde207328176830dff87")
+	def setUp(self):
+		self.foo = Foo() 
+	def test_get_gravatar_url(self):
+		result = self.foo.gravatar_url("example1@gmail.com")
+		self.assertEquals(result, "https://www.gravatar.com/avatar/f3e820cc128ffde207328176830dff87")
  
 if __name__ == "__main__":
- unittest.main()
+	unittest.main()
